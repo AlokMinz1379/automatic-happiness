c516d7760c1ed3cefc5f15596876db3f4c4d5fa8
Alok Minz <alokminz321@gmail.com>
2021-06-11 20:50:26 +0530

    1st code

--- 1st program.py
+++ 1st program.py
@@ -1,1 +1,2 @@
-print ("Hello world.New to programming") \n print ("Let's make career out of this.Awesome stuff")
+print ("Hello world.New to programming\n") 
+print ("Let's make career out of this.Awesome stuff")
